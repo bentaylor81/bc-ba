72617cc397eb8109dfafa594fb2faabd751ad366
Ben Taylor <bentaylor81@gmail.com>
2020-02-15 17:24:29 +0000

    Change settings.py

--- bcba/settings.py
+++ bcba/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-   # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Comment this out when working locally. 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
