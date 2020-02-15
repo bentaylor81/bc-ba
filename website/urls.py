74662a36dfaf03e054aa5334963d4772a4328958
Ben Taylor <bentaylor81@gmail.com>
2020-02-15 14:31:57 +0000

    BCBA full site html uploaded

--- website/urls.py
+++ website/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('who-are-we/', views.who_are_we, name='who-are-we'),
+    path('events/', views.events, name='events'),
+    path('membership/', views.membership, name='membership'),
+    path('membership-types/', views.membership_types, name='membership-types'),
+    path('member-directory/', views.member_directory, name='member-directory'),
+    path('sponsorship/', views.sponsorship, name='sponsorship'),
+    path('current-sponsors/', views.sponsors_current, name='sponsors-current'),
+    path('resources/', views.resources, name='resources'),
+    path('contact/', views.contact, name='contact'),
 ]
--- website/views.py
+++ website/views.py
@@ -3,3 +3,32 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
+def about(request):
+    return render(request, 'about.html', {})
+
+def who_are_we(request):
+    return render(request, 'who-are-we.html', {})
+
+def events(request):
+    return render(request, 'events.html', {})
+
+def membership(request):
+    return render(request, 'membership.html', {})
+
+def membership_types(request):
+    return render(request, 'membership-types.html', {})
+
+def member_directory(request):
+    return render(request, 'member-directory.html', {})
+
+def sponsorship(request):
+    return render(request, 'sponsorship.html', {})
+
+def sponsors_current(request):
+    return render(request, 'sponsors-current.html', {})
+
+def resources(request):
+    return render(request, 'resources.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
