8d5224d4867eda010e937892438a7c27c78aad9a
Ben Taylor <bentaylor81@gmail.com>
2020-02-21 05:25:15 +0000

    Add host to settings.py

---
+++ static/js/scripts.js
@@ -0,0 +1,10 @@
+/* Controls the mobile menu dropdown */
+
+function mobileMenu() {
+    var x = document.getElementById("menu");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
