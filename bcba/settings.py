fbbf36331639f8143616f2123fd324e584a0fef0
Ben Taylor <bentaylor81@gmail.com>
2020-03-27 13:32:28 +0000

    Settings

--- bcba/settings.py
+++ bcba/settings.py
@@ -175,16 +175,9 @@ ACCOUNT_EMAIL_SUBJECT_PREFIX = 'Contact Email Message'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # S3 Buckets Config
-
-
-AWS_ACCESS_KEY_ID = 'AKIA2S2G377Y7UEZ4I65'
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'bcba-v1'
-
-AWS_ACCESS_KEY_ID = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_SECRET_ACCESS_KEY = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_STORAGE_BUCKET_NAME = 'bcba'
-
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
